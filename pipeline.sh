15af5f461a9596855a66fccf20dee6a2034d3823
Max Cruesemann <mcruesemann@ucsd.edu>
2014-10-21 11:20:38 -0700

    added bash script

---
+++ pipeline.sh
@@ -0,0 +1,22 @@
+echo Cleaning up
+rm *.txt
+rm *.tmp
+rm *.csv
+
+echo download data
+curl https://github.com/zonca/swcarpentry-workshop-pandas/blob/master/rawdata/rawdata.zip?raw=true -o rawdata.zip -L
+
+echo unpack data
+unzip rawdata.zip
+rm rawdata.zip
+
+rm *.tmp
+
+for f in *.txt
+do
+mv $f ${f/txt/csv}
+done
+
+echo available files
+ls *.csv
+
